df2a4db98c890680f706fdf065ffdff3d814b7f2
Ty Everett <ty@tyweb.us>
2023-11-02 12:38:21 -0700

    Private Key Generator

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { PrivateKey, PublicKey } from '@bsv/sdk'
 
 const App = () => {
+  const [privateKey, setPrivateKey] = useState('Generate a private key!')
+  const [publicKey, setPublicKey] = useState('Corresponding to the generated private key.')
+  const handleClick = () => {
+    const key = PrivateKey.fromRandom()
+    setPrivateKey(key.toHex())
+    setPublicKey(key.toPublicKey().toDER())
+  }
+
   return (
     <center style={{ margin: '1em' }}>
-      <h1>Hello, Simple App!</h1>
-      <p>This is a simple webpack setup for a React app.</p>
-      <p>Changes to the code are live-reloaded.</p>
+      <h1>Private Key Generator!</h1>
+      <p><b>Private Key</b>: {privateKey}</p>
+      <p><b>Public Key</b>: {publicKey}</p>
+      <button onClick={handleClick}>New Key</button>
     </center>
   )
 }
